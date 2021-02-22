f0f13c7572f4448eb14d6153f1385288d4bd1436
Anusha <anushabt@gmail.com>
2021-02-22 14:46:50 -0600

    Assignment 3 with bonus mission3

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/models/JobData.java
@@ -227,5 +227,11 @@ public class JobData {
         return allCoreCompetency;
     }
 
+    public static ArrayList<Job> getAllJobs() {
+        loadData();
+        allJobs.sort(new NameSorter());
+        return allJobs;
+    }
+
 }
 
